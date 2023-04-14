625306d7efb02440224e7d882ae4120cdb0ca866
Nikki Tenorio <teno0007@algonquinlive.com>
2023-04-14 18:46:23 -0400

    added pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
 import './App.css';
+import LoginLogout from './pages/login-logout/login-logout';
 
 function App() {
   return (
     <div className="App">
-      
+      <header className="App-header">
+      <LoginLogout />
+      </header>
     </div>
   );
 }
---
+++ src/pages/gift-list/gift-list.jsx
@@ -0,0 +1,11 @@
+import './gift-list.css';
+
+function GiftList() {
+  return (
+    <div className="login">
+     <p>Gift List Page</p>
+    </div>
+  );
+}
+
+export default GiftList;
