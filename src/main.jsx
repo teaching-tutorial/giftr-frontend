0ad321f8b75b20722429d78b10713c5c0df057d3
Nikki Tenorio <teno0007@algonquinlive.com>
2023-04-17 18:20:57 -0400

    added login page using googleAuth

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,25 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { TokenProvider } from "./context/TokenContext";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { Routes, Route } from "react-router-dom";
+import People from "./components/People";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  <GoogleOAuthProvider clientId="457188597101-qflpifqkdtorfnmh03qmha2k6i1tr3ks.apps.googleusercontent.com">
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <TokenProvider>
           <App />
         </TokenProvider>
+        <Routes>
+        <Route path="/people" element={<People />} />
+      </Routes>
       </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>
+  </GoogleOAuthProvider>
 );
